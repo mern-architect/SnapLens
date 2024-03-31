c1dce4e8d7e8b86d89c521e095b98716700d0e15
Vijay Pratap Singh <vijaypratap218@gmail.com>
2024-03-31 22:40:08 +0530

    Init

---
+++ SnapLens/SnapLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapLensApp.swift
+//  SnapLens
+//
+//  Created by Vijay Pratap Singh on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
